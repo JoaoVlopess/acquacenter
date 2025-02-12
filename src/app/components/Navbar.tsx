e823cda0e47fdee64529d698e4fe39eb690dca9c
JoaoVlopess <joaovlopes.carvalho@gmail.com>
2025-02-12 16:32:41 -0300

    Navbar transition

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -29,8 +29,8 @@ const Navbar = () => {
             </a>
 
             <div className="">
-                <ul className={` flex-col items-start justify-start fixed top-[75px] right-0 w-80 h-screen bg-blue-900 shadow-lg pt-10 pl-0 pb-0 pr-3 text-white font-medium
-            md:flex-row md:relative md:w-auto md:h-auto md:bg-transparent md:p-0 md:top-0 md:shadow-none ${menu? "hidden" : "flex" }` }>
+                <ul className={` flex flex-col items-start justify-start fixed top-[75px] right-0 w-80 h-screen bg-blue-900 shadow-lg pt-10 pl-0 pb-0 pr-3 text-white font-medium
+            md:flex-row md:relative md:w-auto md:h-auto md:bg-transparent md:p-0 md:top-0 md:shadow-none ${menu? "right-[-320px]" : "right-0" }  transition-all duration-300 ease-in-out` }>
 
                     <li className="mb-7 md:mb-0"><a href="" className="relative px-5 ease-in-out hover:text-gray-300 after:content-[''] after:absolute after:left-[20px] after:bottom-[-4px] after:w-0 after:h-[2px] after:bg-gray-300 after:transition-all after:duration-300 hover:after:w-[30%]">HOME</a></li>
                     <li className="mb-7 md:mb-0"><a href="" className="relative px-5 ease-in-out hover:text-gray-300 after:content-[''] after:absolute after:left-[20px] after:bottom-[-4px] after:w-0 after:h-[2px] after:bg-gray-300 after:transition-all after:duration-300 hover:after:w-[30%]">PRODUTOS</a></li>
